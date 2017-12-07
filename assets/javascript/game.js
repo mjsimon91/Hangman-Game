b50b3ca688fee235c69c6bd3b38f7d3e6a7ea7b1
mjsimon91 <mjsimon91@gmail.com>
2017-12-07 18:26:53 -0500

    Made a number of bug fixes and added the CSS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 // Creating an array that will hold the holiday themed words
 
-var words = ["Christmas", "Chanukkah", "Yule Log", "New Years", "Times Square", "Presents", "Rockefellar Center", "Christmas Tree", "Dreidel","Menorah" ];
+var words = ["Christmas", "Chanukkah", "New Years", "Times Square", "Presents", "Rockefellar Center"];
 
 var wins = 0;
 
@@ -8,58 +8,68 @@ var lettersGuessed;
 var lettersFound;
 var remainingGuesses;
 var currentWord;
-var hiddenCurrentWord;
 var selectedWord;
+var hiddenCurrentWord;
+var audioElement;
 
 // display variables in the HTML
 updateHtml = function() {
 	document.querySelector("#wins").innerHTML = wins;
 	document.querySelector("#letters-guessed").innerHTML = lettersGuessed;
 	document.querySelector("#remaining-guesses").innerHTML = remainingGuesses;
-	document.querySelector("#answer").innerHTML = hiddenCurrentWord;
-};
+	document.querySelector("#answer").innerHTML = hiddenCurrentWord.split('').join(' ');
+}
 
 // initialize variables
 newGame = function() {
 	lettersGuessed = [];
 	lettersFound = [];
-	remainingGuesses = 12;
+	remainingGuesses = 6;
 	hiddenCurrentWord = "";
-	currentWord = words[Math.floor(Math.random()*words.length)];
-	selectedWord = currentWord.toLowerCase()
+	selectedWord = words[Math.floor(Math.random()* words.length)];
+	currentWord = selectedWord.toLowerCase();
+	console.log(selectedWord);
 
-	for (var i = 0; i < selectedWord.length; i++) {
-		if (selectedWord.charAt(i) != ' ') {
-			hiddenCurrentWord += "_";
+
+	for (var i = 0; i < currentWord.length; i++) { 	//creating a loop to find the number of characters to display on screen
+		if (currentWord.charAt(i) != ' ') { 					//For each character, look if there is not a space
+			hiddenCurrentWord += "_";										// if there is a character, then display an underscore
 		} else {
-			hiddenCurrentWord += "&nbsp" + "&nbsp";
+			hiddenCurrentWord += " ";										//otherwise, displaye a non breaking space
 		}
 	}
-};
+}
 
-newGame();
-updateHtml();
+newGame();																				// Run the newGame function
+updateHtml();																			// Run the updateHtml function
 
 // handle a user's guess every time they type a letter
 document.onkeyup = function(event) {
-	if (event.keyCode < 65 || event.keyCode > 90) {
+	if (event.keyCode < 65 || event.keyCode > 90) {	// Making sure that each character is actually a character and not a number or space
 		// the typed key is not a letter
-		return;
+		return;																				// End this function if it is as there is no point looping through the rest
 	}
 
 	var userGuess = event.key;
-	lettersGuessed.push(userGuess);
+
 	var found = false;
+	console.log(lettersFound);
+	console.log(lettersGuessed);
 
-	if (lettersFound.includes(userGuess)) {
-		alert("You have already guessed the letter: " + userGuess);
-		return;
+	if (lettersFound.includes(userGuess) ||
+			lettersGuessed.includes(userGuess)) {
+				alert("You have already guessed this letter.");
+				return;
 	}
+	lettersGuessed.push(userGuess.toUpperCase());
+//Creating a loop to go through each letter in the currentWord and determine if the userGuess belongs in place of the underscore
 
-	for (var i = 0; i < selectedWord.length; i++) {
-		if (selectedWord.charAt(i) == userGuess) {
-			// correct guess
-			hiddenCurrentWord = hiddenCurrentWord.substr(0, i) + userGuess + hiddenCurrentWord.substr(i + 1);
+	for (var i = 0; i < currentWord.length; i++) {
+		if (currentWord.charAt(i) == userGuess) {
+
+			// correct guess... Go through each character in the index until you reach the character, then substitute with the userGuess
+
+			hiddenCurrentWord = hiddenCurrentWord.substr(0, i) + userGuess.toUpperCase() + hiddenCurrentWord.substr(i + 1);
 			found = true;
 			lettersFound.push(userGuess);
 		}
@@ -69,17 +79,23 @@ document.onkeyup = function(event) {
 		// incorrect guess
 		remainingGuesses--;
 		if (remainingGuesses == 0) {
-			// the user lost this game
+
+			// the user lost this game and will now see the word
+
+			alert("Oh no! You did not figure out that the word was " + selectedWord);
 			newGame();
 		}
 	}
 
-	if (selectedWord.replace(/\s/g, "").length == lettersFound.length) {
+	if (currentWord.replace(/\s/g, "").length == lettersFound.length) {
 		// the user won this game
 		wins++;
-    alert("Wahoo! You saved the day!")
+
+		alert("Winner!")
 		newGame();
 	}
 
+
 	updateHtml();
+
 }
